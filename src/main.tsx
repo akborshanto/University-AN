8dc5bc30947203397835eee0bef3969baa3782c4
akbor shanto <akborshanto1111.com>
2025-01-23 21:40:39 +0600

    redux implement

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes.tsx';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.ts';
+import { store } from './redux/api/feture/store.ts';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <Provider store={store}> <RouterProvider router={router} /></Provider>
+     
+   
   </React.StrictMode>
 );
